0b4b5655d38414451c39eeec1d69c99579eafad9
NatalliaNikitsina <natknik73@gmail.com>
2023-09-13 21:17:34 +0300

    task2 finished

---
+++ task_2.py
@@ -0,0 +1,24 @@
+"""Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6),
+которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки 
+и столбца. Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, 
+которые должны быть распечатаны. Нумерация строк и столбцов идет с единицы (подумайте,
+                                                                     почему не с нуля). 
+
+
+Примечание: бинарной операцией называется любая операция, у которой ровно два аргумента, как, например, у операции умножения.
+
+*Пример:*
+
+**Ввод:** `print_operation_table(lambda x, y: x * y) ` 
+**Вывод:**
+1 2 3 4 5 6"""
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    for row in range(1, num_rows + 1):
+        row_value = []
+        for col in range(1, num_columns + 1):
+            result = operation(row, col)
+            row_value.append(result)
+        print(*row_value)
+
+print_operation_table(lambda x, y: x * y)
